272347a76386cfee2cf663e5f4289ab47684429d
gyt <1987786006@qq.com>
2017-12-15 10:39:38 +0800

    hello

---
+++ perceptron_learning/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @TIME     : 2017/12/14 21:02
+# @Author  : Gyt
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from perceptron import Perceptron
+
+if __name__ == "__main__":
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+                     'machine-learning-databases/iris/iris.data', header=None)
+    print(df.tail())
+
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0, 2]].values
+    plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+    plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+    plt.xlabel('petal length')
+    plt.ylabel('sepal length')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    ppn = Perceptron(eta=0.1, n_iter=10)
+    ppn.fit(X, y)
+    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_,marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel('Number of misclassifications')
+    plt.show()
