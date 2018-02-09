ce40a8d95a8c3a53813a2fc87ad9f9ebdbed9ef8
gyt <1987786006@qq.com>
2018-02-09 22:05:46 +0800

    svm

---
+++ chapter_3/sigmoid/regularization.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @TIME     : 2017/12/28 20:50
+# @Author  : Gyt
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot all samples
+    X_test, y_test = X[test_idx, :], y[test_idx]
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+    # highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='k',
+                    alpha=0.2, linewidth=1, marker='o',
+                    s=85, label='test set')
+
+
+if __name__ == '__main__':
+    iris = datasets.load_iris()
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+    X_combined_std = np.vstack((X_train_std, X_test_std))
+    y_combined = np.hstack((y_train, y_test))
+
+    lr = LogisticRegression(C=1000.0, random_state=0)
+    lr.fit(X_train_std, y_train)
+    print(lr.predict_proba(X_test_std[0, :]))
+    plot_decision_regions(X_combined_std, y_combined,
+                          classifier=lr,
+                          test_idx=range(105, 150))
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    weights, params = [], []
+    for c in np.arange(-5, 5):
+        lr = LogisticRegression(C=10.0**c, random_state=0)
+        lr.fit(X_train_std, y_train)
+        weights.append(lr.coef_[1])
+        params.append(10.0 ** c)
+    weights = np.array(weights)
+    plt.plot(params, weights[:, 0],label = 'petal length')
+    plt.plot(params, weights[:, 1], linestyle='--',label = 'petal width')
+    plt.ylabel('weight coefficient')
+    plt.xlabel('C')
+    plt.legend(loc='upper left')
+    plt.xscale('log')
+    plt.show()
